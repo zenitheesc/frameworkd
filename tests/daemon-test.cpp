5291243961d80fc6a63c61da0ec4e16009317c95
Maíra Canal <maira.canal@usp.br>
2021-10-06 13:39:37 +0000

    unit test: adding get and set unit tests

--- tests/daemon-test.cpp
+++ tests/daemon-test.cpp
@@ -4,28 +4,40 @@
 #include <gtest/gtest.h>
 #include <thread>
 
-PathHandler::DBusPath m_methodPath = {
+DBusHandler::Path m_methodPath {
     "org.sdbuscpp.concatenator",
     "/org/sdbuscpp/concatenator",
     "org.sdbuscpp.Concatenator",
     "concatenate"
 };
-PathHandler::DBusPath m_signalPath = {
+
+DBusHandler::Path m_signalPath {
     "org.sdbuscpp.concatenator",
     "/org/sdbuscpp/concatenator",
     "org.sdbuscpp.Concatenator",
     "concatenated"
 };
 
+DBusHandler::Path m_propertiesPath {
+    "org.sdbuscpp.concatenator",
+    "/org/sdbuscpp/concatenator",
+    "org.sdbuscpp.Concatenator",
+    "properties"
+};
+
 bool isServerConfig;
+nlohmann::json m_properties;
 
 class ServerClient : public ::testing::Test {
 public:
-    DBusHandler m_client;
+    DBusHandler m_client { m_methodPath.service, false };
 
     static void SetUpTestSuite()
     {
         isServerConfig = true;
+
+        m_properties["num"] = 9;
+
         std::thread t1(&ServerClient::server);
         t1.detach();
         while (isServerConfig) { };
@@ -34,14 +46,18 @@ public:
     static void server()
     {
         DBusHandler server(m_methodPath.service);
+
         server.registerSignal(m_signalPath);
         server.registerMethod(m_methodPath, [&](nlohmann::json req) {
             nlohmann::json res;
             int num = req["num"];
+
             res["num"] = num * 2;
             server.emitSignal(m_signalPath, res);
             return res;
         });
+        server.exposeProperty(
+            m_propertiesPath, [&]() { return m_properties; }, [&](nlohmann::json req) { m_properties = req; });
 
         isServerConfig = false;
         server.finish();
@@ -63,7 +79,6 @@ TEST_F(ServerClient, Method)
 TEST_F(ServerClient, AsyncMethod)
 {
     nlohmann::json arg;
-    bool isCalled = false;
     int num = 4;
 
     arg["num"] = num;
@@ -71,9 +86,7 @@ TEST_F(ServerClient, AsyncMethod)
     m_client.finish();
     m_client.callMethodAsync(m_methodPath, arg, [&](nlohmann::json res) {
         EXPECT_EQ(num * 2, res["num"]);
-        isCalled = true;
     });
-    while (!isCalled) { }
 }
 
 TEST_F(ServerClient, Signal)
@@ -81,12 +94,45 @@ TEST_F(ServerClient, Signal)
     nlohmann::json arg;
     nlohmann::json res;
     int num = 4;
+
     m_client.subscribeToSignal(m_signalPath, [&](nlohmann::json rec) {
         EXPECT_EQ(num * 2, rec["num"]);
     });
+
     m_client.finish();
     arg["num"] = num;
 
     res = m_client.callMethod(m_methodPath, arg);
+}
+
+TEST_F(ServerClient, Get)
+{
+    m_client.finish();
+
+    nlohmann::json res = m_client.getProperty(m_propertiesPath);
+
+    EXPECT_EQ(res, m_properties);
+}
+
+TEST_F(ServerClient, AsyncGet)
+{
+    m_client.finish();
+
+    m_client.getProperty(m_propertiesPath, [&](nlohmann::json arg) {
+        EXPECT_EQ(arg, m_properties);
+    });
+}
+
+TEST_F(ServerClient, Set)
+{
+    nlohmann::json arg;
+
+    arg["num"] = (int)m_properties["num"] * 4;
+
+    m_client.finish();
+
+    m_client.setProperty(m_propertiesPath, arg);
+
+    EXPECT_EQ(arg, m_properties);
 }
 
