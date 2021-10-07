0f8c526ef38168523d5fb8086b49898eb2e401b3
CarlosCraveiro <carlos.craveiro@usp.br>
2021-10-07 18:34:24 -0300

    Adding JavDocs for service-proxy

--- src/classes/service-proxy/service-proxy.hpp
+++ src/classes/service-proxy/service-proxy.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "../service/service.hpp"
+#include <memory>
 #include <mutex>
 #include <nlohmann/json.hpp>
 #include <thread>
-#include <memory>
 
 class Status {
 public:
+    /*enum for the possible service-thread states*/
     enum stateT {
         MISSINGDEPENDENCIES = 0,
         UNINITIALIZED,
@@ -17,36 +18,97 @@ public:
     };
 
 private:
+    /*state from @ref stateT enum*/
     stateT m_state;
+
+    /*class' mutex to ensure it's thread safe*/
     std::mutex m_mtx;
 
 public:
-	auto getState()->stateT;
-	void setState(stateT newState);
-};
+    /**
+ 	 * @brief	thread-safe getter to inside member @ref m_state
+ 	 * 
+ 	 * @return	stateT state that's inside the class.
+ 	 */
+    auto getState() -> stateT;
 
+    /**
+	 * @brief	thread-safe setter to the inside member @ref m_state
+	 *
+	 * @param	stateT state that will be the new state of the class.
+	 */
+    void setState(stateT newState);
+};
 
 class ServiceProxy {
 private:
-	IService& m_innerService;
+    /* a reference for a Service implemented by the user */
+    IService& m_innerService;
+
+    /* structure made to allow the use of the service-proxy 
+	 * conf-file in multiples threads.
+	 */
     struct SafeJson {
         nlohmann::json data;
         std::mutex mtx;
     };
+
+    /* json thread-safe that will contain the service-proxy conf-file */
     SafeJson m_dependencies;
+
+    /* class made to keep track of innerThread current state */
     Status m_status;
+
+    /* thread object that will contain the running thread */
     std::thread m_innerThread;
 
 public:
-    auto getStatus()->nlohmann::json;
+    /**
+	 * @brief	returns a json with the general status + information 
+	 * 			about the service-proxy's innerThread and its innerService.
+	 */
+    auto getStatus() -> nlohmann::json;
+
+    /* Function that will update the inner dependencies along changes in their states */
     //    nlohmann::json update(void);
-	ServiceProxy(IService& service, nlohmann::json configs);
+
+    /**
+	 * @brief	class' constructor
+	 *
+	 * @param	service IService class passed by reference
+	 *
+	 * @param	configs nlohmann::json that contains the configure
+	 * 			files that refer to the service-proxy and 
+	 * 			its @ref innerService
+	 */
+    ServiceProxy(IService& service, nlohmann::json configs);
+
+    /**
+	 * @brief	class' destructor
+	 */
     ~ServiceProxy();
 
-//private:
+    //private:
+
+    /**
+ 	 * @brief	starts running the @ref m_innerService in 
+ 	 * 			a thread that will be in @ref m_innerThread, 
+	 * 			and that is encapsulated with @ref servicePod.
+ 	 */
     void run();
+
+    /**
+ 	 * @brief	stops a running thread "located" in 
+	 * 			@ref m_innerThread that contains an @ref m_innerService
+	 * 			and that is encapsulated with @ref servicePod 
+ 	 */
     void stop();
-private:    
-	static void servicePod(IService& service, Status& status);
+
+private:
+    /**
+ 	 * @brief	function that encapsulates the class @ref IService
+	 * 			to be runned in a thread as explicit in @ref run
+ 	 */
+    static void servicePod(IService& service, Status& status);
 };
 
