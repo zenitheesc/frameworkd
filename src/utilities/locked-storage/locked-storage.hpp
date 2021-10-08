7c339ae6e9fb209d51f97ba86f9d614ad6f31a3f
Arthur Grillo <arthur.grillo@usp.br>
2021-10-08 19:00:44 -0300

    Making the locked storagare library header only

--- src/utilities/locked-storage/locked-storage.hpp
+++ src/utilities/locked-storage/locked-storage.hpp
@@ -21,12 +21,48 @@ public:
 
     void operator=(const LockedStorage&) = delete;
 
-    static auto GetInstance() -> LockedStorage*;
-
-    auto operator[](std::string) -> nlohmann::json;
+    static auto GetInstance() -> LockedStorage&;
 
     template <typename T>
-    void set(std::string, T);
+    void set(const std::string&, T);
 
-    auto get(std::string) -> nlohmann::json;
+    auto get(const std::string&) -> nlohmann::json;
 };
+
+LockedStorage* LockedStorage::s_instance { nullptr };
+std::mutex LockedStorage::s_mutex;
+nlohmann::json LockedStorage::s_storage;
+
+/**
+ * @brief Retrieves the instance of the LockedStorage  
+ */
+auto LockedStorage::GetInstance() -> LockedStorage&
+{
+    std::lock_guard<std::mutex> lock(s_mutex);
+    if (s_instance == nullptr) {
+        s_instance = new LockedStorage;
+    }
+    return *s_instance;
+}
+
+/**
+ * @brief Get the value from the storage
+ * @param field name from the field whre the requested value is
+ */
+auto LockedStorage::get(const std::string& field) -> nlohmann::json
+{
+    std::lock_guard<std::mutex> lock(s_mutex);
+    return s_storage[field];
+}
+
+/**
+ * @brief Write a value to the storage
+ * @param field Location to write
+ * @param value Value to be writed 
+ */
+template <typename T>
+void LockedStorage::set(const std::string& field, T value)
+{
+    std::lock_guard<std::mutex> lock(s_mutex);
+    s_storage[field] = value;
+}
