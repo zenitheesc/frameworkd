f7f98810f0241847ce10dd735c9285fd2f3b123e
CarlosCraveiro <carlos.craveiro@usp.br>
2021-10-23 03:56:35 -0300

    service-proxy: Beginning the refactor of class' header-file

--- src/classes/service-proxy/service-proxy.hpp
+++ src/classes/service-proxy/service-proxy.hpp
@@ -1,120 +1,74 @@
 #pragma once
 #include "../service/service.hpp"
-#include <memory>
+#include <map>
 #include <mutex>
 #include <nlohmann/json.hpp>
 #include <thread>
-#include <utility>
+
+class ServiceProxy;
 
 class Status {
 public:
-    /*enum for the possible service-thread states*/
-    enum stateT {
-        MISSING_DEPENDENCIES = 0,
+    enum statusT { MISSING_DEPENDENCIES = 0,
         UNINITIALIZED,
-        INITIALIZED,
         RUNNING,
         STOPPED,
-        DEAD
-    };
+        FINISHED,
+        STAND_BY,
+        UNKNOWN };
 
-private:
-    /*state from @ref stateT enum*/
-    stateT m_state;
+protected:
+    statusT m_state;
+    ServiceProxy* m_upperProxy;
+    std::mutex m_statusMtx;
 
-    /*class' mutex to ensure it's thread safe*/
-    std::mutex m_mtx;
-
-public:
-    /**
- 	 * @brief	thread-safe getter to inside member @ref m_state
- 	 * 
- 	 * @return	stateT state that's inside the class.
- 	 */
-    auto getState() -> stateT;
-
-    /**
-	 * @brief	thread-safe setter to the inside member @ref m_state
-	 *
-	 * @param	stateT state that will be the new state of the class.
-	 */
-    void setState(stateT newState);
+    virtual void someThingIsMissing() { }
+    virtual void allFine() { }
+    auto getStatus() -> statusT;
 };
 
 class ServiceProxy {
-private:
-    /* a reference for a Service implemented by the user */
-    IService& m_realService;
+public:
+    enum proxyT { staticService = 0,
+        routineService };
 
-    /* structure made to allow the use of the service-proxy 
-	 * conf-file in multiples threads.
-	 */
-    class SafeJson {
+protected:
+    class ProxyConfigs {
+        struct Dependencie {
+            Status::statusT m_reqrState;
+            Status::statusT m_currState;
+        };
+
+        std::map<std::string, Dependencie> m_depsMap;
+
     public:
-        nlohmann::json m_data;
-        std::mutex m_mtx;
-        SafeJson(nlohmann::json data)
-            : m_data(data)
-        {
-        }
+        void changeDep(std::string dependencieId, Status::statusT currState);
+        explicit ProxyConfigs(std::map<std::string, Status::statusT> depsMap);
     };
 
-    /* json thread-safe that will contain the service-proxy conf-file */
-    SafeJson m_dependencies;
+    proxyT m_proxyType;
+    ProxyConfigs m_proxyConfigs;
+    Status* m_state;
+    IService& m_realService;
 
-    /* class made to keep track of innerThread current state */
-    Status m_status;
+    virtual void serviceCycle() { }
+    virtual void changeState() { }
+    virtual void autoUpdate() { }
+    auto reportStatus() -> nlohmann::json;
 
-    /* thread object that will contain the running thread */
-    std::thread m_innerThread;
-
-public:
-    /**
-	 * @brief	returns a json with the general status + information 
-	 * 			about the service-proxy's innerThread and its innerService.
-	 */
-    auto getStatus() -> nlohmann::json;
-
-    /* Function that will update the inner dependencies along changes in their states */
-    //    nlohmann::json update(void);
-
-    /**
-	 * @brief	class' constructor
-	 *
-	 * @param	service IService class passed by reference
-	 *
-	 * @param	configs nlohmann::json that contains the configure
-	 * 			files that refer to the service-proxy and 
-	 * 			its @ref innerService
-	 */
-    ServiceProxy(IService& service, nlohmann::json configs);
-
-    /**
-	 * @brief	class' destructor
-	 */
-    ~ServiceProxy();
-
-    //private:
-
-    /**
- 	 * @brief	starts running the @ref m_innerService in 
- 	 * 			a thread that will be in @ref m_innerThread, 
-	 * 			and that is encapsulated with @ref servicePod.
- 	 */
-    void run();
-
-    /**
- 	 * @brief	stops a running thread "located" in 
-	 * 			@ref m_innerThread that contains an @ref m_innerService
-	 * 			and that is encapsulated with @ref servicePod 
- 	 */
-    void stop();
-
-private:
-    /**
- 	 * @brief	function that encapsulates the class @ref IService
-	 * 			to be runned in a thread as explicit in @ref run
- 	 */
-    void servicePod();
+    explicit ServiceProxy(IService& realService, std::map<std::string, Status::statusT> depsMap);
 };
 
+namespace staticService {
+class Proxy;
+
+class Proxy : public ServiceProxy {
+public:
+};
+}
+
+namespace routineService {
+class Proxy : public ServiceProxy {
+public:
+};
+}
