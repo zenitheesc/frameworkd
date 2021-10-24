8aedc92e685e5ce3ac7ca2a873c8e530fa0b472f
CarlosCraveiro <carlos.craveiro@usp.br>
2021-10-24 20:24:55 -0300

    ServiceProxy: refactoring ".h" with state design pattern

--- src/classes/service-proxy/service-proxy.hpp
+++ src/classes/service-proxy/service-proxy.hpp
@@ -9,7 +9,7 @@ class ServiceProxy;
 
 class Status {
 public:
-    enum statusT { MISSING_DEPENDENCIES = 0,
+    enum stateT { MISSING_DEPENDENCIES = 0,
         UNINITIALIZED,
         RUNNING,
         STOPPED,
@@ -18,13 +18,16 @@ public:
         UNKNOWN };
 
 protected:
-    statusT m_state;
-    ServiceProxy* m_upperProxy;
+    const stateT m_state;
     std::mutex m_statusMtx;
 
-    virtual void someThingIsMissing() { }
+    virtual void somethingIsMissing() { }
     virtual void allFine() { }
-    auto getStatus() -> statusT;
+    auto getState() -> stateT;
+    explicit Status(stateT state)
+        : m_state(state)
+    {
+    }
 };
 
 class ServiceProxy {
@@ -35,40 +38,150 @@ public:
 protected:
     class ProxyConfigs {
         struct Dependencie {
-            Status::statusT m_reqrState;
-            Status::statusT m_currState;
+            Status::stateT m_reqrState;
+            Status::stateT m_currState;
         };
 
         std::map<std::string, Dependencie> m_depsMap;
 
     public:
-        void changeDep(std::string dependencieId, Status::statusT currState);
-        explicit ProxyConfigs(std::map<std::string, Status::statusT> depsMap);
+        void changeDep(std::string dependencieId, Status::stateT currState);
+        explicit ProxyConfigs(std::map<std::string, Status::stateT> depsMap);
     };
 
     proxyT m_proxyType;
     ProxyConfigs m_proxyConfigs;
-    Status* m_state;
     IService& m_realService;
 
     virtual void serviceCycle() { }
     virtual void changeState() { }
     virtual void autoUpdate() { }
-    auto reportStatus() -> nlohmann::json;
+    auto reportStatus() -> nlohmann::json { return { {} }; }
 
-    explicit ServiceProxy(IService& realService, std::map<std::string, Status::statusT> depsMap);
+    explicit ServiceProxy(IService& realService, std::map<std::string, Status::stateT> depsMap);
 };
 
 namespace staticService {
 class Proxy;
 
-class Proxy : public ServiceProxy {
+class SStatus : public Status {
+protected:
+    const Proxy* m_upperProxy;
+
 public:
+    explicit SStatus(stateT state)
+        : Status(state)
+    {
+    }
 };
-}
+
+class MissingDependencies : public SStatus {
+public:
+    explicit MissingDependencies()
+        : SStatus(MISSING_DEPENDENCIES)
+    {
+    }
+    void allFine() override;
+};
+
+class Uninitialized : public SStatus {
+public:
+    explicit Uninitialized()
+        : SStatus(UNINITIALIZED)
+    {
+    }
+    void somethingIsMissing() override;
+};
+
+class Running : public SStatus {
+public:
+    explicit Running()
+        : SStatus(RUNNING)
+    {
+    }
+};
+
+class StandBy : public SStatus {
+public:
+    explicit StandBy()
+        : SStatus(STAND_BY)
+    {
+    }
+    void somethingIsMissing() override;
+};
+
+class Proxy : public ServiceProxy {
+protected:
+    bool m_runnedOnce;
+    SStatus* m_status;
+
+public:
+    void execute() { serviceCycle(); } // Temporary!!!
+};
+} // namespace staticService
 
 namespace routineService {
-class Proxy : public ServiceProxy {
+class Proxy;
+class RStatus : public Status {
 public:
+    std::mutex m_borrowMtx;
+    Proxy* m_upperProxy;
+
+    explicit RStatus(stateT state)
+        : Status(state)
+    {
+    }
 };
-}
+
+class MissingDependencies : public RStatus {
+public:
+    explicit MissingDependencies()
+        : RStatus(MISSING_DEPENDENCIES)
+    {
+    }
+
+    void allFine() override;
+};
+
+class StandBy : public RStatus {
+public:
+    explicit StandBy()
+        : RStatus(STAND_BY)
+    {
+    }
+};
+
+class Running : public RStatus {
+public:
+    explicit Running()
+        : RStatus(RUNNING)
+    {
+    }
+    
+    void somethingIsMissing() override;
+};
+
+class Stopped : public RStatus {
+public:
+    explicit Stopped()
+        : RStatus(STOPPED)
+    {
+    }
+};
+
+class Finished : public RStatus {
+public:
+    explicit Finished()
+        : RStatus(FINISHED)
+    {
+    }
+};
+
+class Proxy : public ServiceProxy {
+protected:
+    std::thread m_thread;
+    void weave();
+    void cut();
+};
+} // namespace routineService
+
