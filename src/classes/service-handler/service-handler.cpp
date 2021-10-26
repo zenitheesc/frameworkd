1740a1c8d3341344022d3e079f35b982dd83a38f
CarlosCraveiro <carlos.craveiro@usp.br>
2021-10-26 20:22:44 -0300

    ServiceHandler: Refactoring to combine with the new ServiceProxys

--- src/classes/service-handler/service-handler.cpp
+++ src/classes/service-handler/service-handler.cpp
@@ -1,37 +1,48 @@
 #include "service-handler.hpp"
 
-//
-//    void buildServiceProxy(IService& userService);
-//    explicit ServiceHandler(nlohmann::json servicesConfigs);
-
 ServiceHandler::ServiceHandler(nlohmann::json servicesConfigs)
-    : m_servicesConfigs(servicesConfigs)
-
 {
+    //JSON DE CONFIGS
+    for (nlohmann::json::iterator it = servicesConfigs["proxys"].begin(); it != servicesConfigs["proxys"].end(); it++) {
+        std::map<std::string, Status::stateT> depsMap;
+
+        for (nlohmann::json::iterator itr = it.value().begin(); itr != it.value().end(); itr++) {
+            depsMap.insert(std::pair<std::string, Status::stateT>(itr.key(), itr.value()));
+        }
+        m_proxyDepsMap.insert(std::pair<std::string, std::map<std::string, Status::stateT>>(it.key(), depsMap));
+    }
 }
 
-void ServiceHandler::buildServiceProxy(IService& userService)
+void ServiceHandler::buildServiceProxy(IService& userService, ServiceProxy::proxyT proxyType)
 {
+    if (proxyType == ServiceProxy::STATIC_SERVICE) {
+        m_serviceMap.insert(std::pair<std::string, ServiceProxy*>(userService.m_serviceId, new staticService::Proxy(userService, m_proxyDepsMap.at(userService.m_serviceId))));
 
-    m_serviceMap.emplace(std::piecewise_construct,
-        std::forward_as_tuple(userService.m_serviceId),
-        std::forward_as_tuple(userService, m_servicesConfigs));
+    } else {
+        m_serviceMap.insert(std::pair<std::string, ServiceProxy*>(userService.m_serviceId, new routineService::Proxy(userService, m_proxyDepsMap.at(userService.m_serviceId))));
+    }
 }
 
 auto ServiceHandler::getProxyStatus(std::string serviceId) -> nlohmann::json
 {
-    nlohmann::json requiredStatus = m_serviceMap.at(serviceId).getStatus();
+    nlohmann::json requiredStatus = m_serviceMap.at(serviceId)->reportStatus();
     return requiredStatus;
 }
 
 auto ServiceHandler::getAllProxyStatus() -> nlohmann::json
 {
-    std::map<std::string, ServiceProxy>::iterator itr;
+    std::map<std::string, ServiceProxy*>::iterator itr;
     nlohmann::json allStatus;
-    for(itr = m_serviceMap.begin(); itr != m_serviceMap.end(); itr++) {
-        allStatus[itr->first] = itr->second.getStatus();
+    for (itr = m_serviceMap.begin(); itr != m_serviceMap.end(); itr++) {
+        allStatus[itr->first] = itr->second->reportStatus();
     }
 
     return allStatus;
 }
 
+ServiceHandler::~ServiceHandler() {
+    std::map<std::string, ServiceProxy*>::iterator itr;
+    for (itr = m_serviceMap.begin(); itr != m_serviceMap.end(); itr++) {
+        delete itr->second; 
+    }
+}
--- src/classes/service-handler/service-handler.hpp
+++ src/classes/service-handler/service-handler.hpp
@@ -8,14 +8,15 @@
 class ServiceHandler {
 //private:
 public:
-    std::map<std::string, ServiceProxy> m_serviceMap;
-    nlohmann::json m_servicesConfigs;
+    std::map<std::string, ServiceProxy*> m_serviceMap;
+    std::map<std::string, std::map<std::string, Status::stateT>> m_proxyDepsMap;
 
 public:
     auto getProxyStatus(std::string serviceId) -> nlohmann::json;
     auto getAllProxyStatus() -> nlohmann::json;
 
-    void buildServiceProxy(IService& userService);
+    void buildServiceProxy(IService& userService, ServiceProxy::proxyT proxyType);
     explicit ServiceHandler(nlohmann::json servicesConfigs);
+    ~ServiceHandler();
 };
 
