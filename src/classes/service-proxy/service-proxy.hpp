636ba83849ab9a085337f43a227b0b8981fa75ac
Carlos Veras <carlos.craveiro@usp.br>
2021-11-08 03:11:29 -0300

    ServiceProxy: splits the class and ties up loose ends
    This commit will be followed by other two, because the gigantic file
    service-proxy.hpp and its .cpp was cut each one in three.
    
    Basically the classes staticService::Proxy and routineService::Proxy and
    their states now exists in their own files. The staticService::Proxy now
    is StaticServiceProxy and routineService::Proxy RoutineServiceProxy,
    the external namespaces was erased and now the states uses the namespace
    of their own classes.
    
    Besides these changes, the new sliced files had their "loose ends" tied
    up, with "loose ends" meaning the several "public", "protected" tags
    that haunted the three classes before, despises the other several
    "//Temporary!" comments which made the life of code reviewers an endless
    nightmare. The solution for all these problems came from turning the
    inherited proxys friends of ServiceHandler, and those friends of an
    test class that I uses as an "unit test" class, so I could test the
    features without spreading MacGyvers through the classes.

--- src/classes/service-proxy/service-proxy.hpp
+++ src/classes/service-proxy/service-proxy.hpp
@@ -1,48 +1,48 @@
 #pragma once
-#include "../service/service.hpp"
+#include "../iservice/iservice.hpp"
 #include <map>
 #include <mutex>
 #include <nlohmann/json.hpp>
 #include <thread>
 
-class ServiceProxy;
-
-class Status {
-public:
-    enum stateT { MISSING_DEPENDENCIES = 0,
-        UNINITIALIZED,
-        RUNNING,
-        STOPPED,
-        FINISHED,
-        STAND_BY,
-        UNKNOWN };
-
-protected:
-    const stateT m_state;
-    std::mutex* const m_statusMtx;
-
-    virtual void somethingIsMissing() { }
-    virtual void allFine() { }
-    auto getState() -> stateT;
-    Status(stateT state, std::mutex* const statusMtx)
-        : m_state(state)
-        , m_statusMtx(statusMtx)
-    {
-    }
-};
-
 class ServiceProxy {
-public:
+protected:
+
+    friend class ServiceHandler;
+    friend class Tester;
+
     enum proxyT { STATIC_SERVICE = 0,
         ROUTINE_SERVICE };
 
-protected:
+    class ServiceState {
+    public:
+        enum stateT { MISSING_DEPENDENCIES = 0,
+            UNINITIALIZED,
+            RUNNING,
+            STOPPED,
+            FINISHED,
+            STAND_BY,
+            UNKNOWN };
+
+        //    protected:
+        const stateT m_state;
+
+        virtual void somethingIsMissing() { }
+        virtual void allFine() { }
+        [[nodiscard]] auto getState() const -> stateT;
+
+        explicit ServiceState(stateT state)
+            : m_state(state)
+        {
+        }
+    };
+
     class ProxyConfigs {
     public:
         struct Dependencie {
-            Status::stateT m_reqrState;
-            Status::stateT m_currState;
-            Dependencie(Status::stateT reqrState, Status::stateT currState)
+            ServiceState::stateT m_reqrState;
+            ServiceState::stateT m_currState;
+            Dependencie(ServiceState::stateT reqrState, ServiceState::stateT currState)
                 : m_reqrState(reqrState)
                 , m_currState(currState)
             {
@@ -53,31 +53,24 @@ protected:
 
         std::map<std::string, Dependencie> m_depsMap;
 
-        void changeDep(std::string dependencieId, Status::stateT currState);
-        explicit ProxyConfigs(std::map<std::string, Status::stateT> depsMap);
+        void changeDep(std::string dependencieId, ServiceState::stateT currState);
+        explicit ProxyConfigs(std::map<std::string, ServiceState::stateT> depsMap);
     };
 
     proxyT m_proxyType;
     std::mutex m_statusMtx;
-
-public://temporary
     ProxyConfigs m_proxyConfigs;
-protected:
     std::string m_realServiceId;
     IService& m_realService;
 
-    virtual void changeState(Status::stateT newState) { }
-
-public://temporary
+    virtual void changeState(ServiceState::stateT newState) { }
     virtual void autoUpdate() { }
     virtual void serviceCycle() { }
 
-protected:
-
 public:
-    virtual auto reportStatus() -> nlohmann::json { return { {} }; }
+    virtual auto reportState() -> nlohmann::json { return { {} }; }
 
-    ServiceProxy(IService& realService, proxyT proxyType, std::map<std::string, Status::stateT> depsMap)
+    ServiceProxy(IService& realService, proxyT proxyType, std::map<std::string, ServiceState::stateT> depsMap)
         : m_realService(realService)
         , m_proxyType(proxyType)
         , m_proxyConfigs(depsMap)
@@ -87,170 +80,4 @@ public:
 
     virtual ~ServiceProxy() = default;
 };
-
-namespace staticService {
-class Proxy;
-
-class SStatus : public Status {
-    friend class Proxy;
-
-protected:
-    Proxy* const m_upperProxy;
-
-public:
-    SStatus(stateT state, Proxy* const upperProxy, std::mutex* const statusMtx)
-        : Status(state, statusMtx)
-        , m_upperProxy(upperProxy)
-    {
-    }
-};
-
-class MissingDependencies : public SStatus {
-public:
-    explicit MissingDependencies(Proxy* const upperProxy, std::mutex* const statusMtx)
-        : SStatus(MISSING_DEPENDENCIES, upperProxy, statusMtx)
-    {
-    }
-    void allFine() override;
-};
-
-class Uninitialized : public SStatus {
-public:
-    explicit Uninitialized(Proxy* const upperProxy, std::mutex* const statusMtx)
-        : SStatus(UNINITIALIZED, upperProxy, statusMtx)
-    {
-    }
-    void somethingIsMissing() override;
-};
-
-class Running : public SStatus {
-public:
-    explicit Running(Proxy* const upperProxy, std::mutex* const statusMtx)
-        : SStatus(RUNNING, upperProxy, statusMtx)
-    {
-    }
-};
-
-class StandBy : public SStatus {
-public:
-    explicit StandBy(Proxy* const upperProxy, std::mutex* const statusMtx)
-        : SStatus(STAND_BY, upperProxy, statusMtx)
-    {
-    }
-    void somethingIsMissing() override;
-};
-
-class Proxy : public ServiceProxy {
-    friend class MissingDependencies;
-    friend class Uninitialized;
-    friend class Running;
-    friend class StandBy;
-
-protected:
-    bool m_runnedOnce;
-    SStatus* m_status;
-
-    void changeState(Status::stateT newState) override;
-public:
-    void serviceCycle() override;
-protected:
-    void autoUpdate() override;
-    auto checkState() -> Status::stateT;
-
-public:
-    Proxy(IService& realService, std::map<std::string, Status::stateT> depsMap);
-    ~Proxy() override;
-
-    auto reportStatus() -> nlohmann::json override;
-    void execute() { serviceCycle(); } // Temporary!!!
-};
-} // namespace staticService
-
-namespace routineService {
-class Proxy;
-class RStatus : public Status {
-    friend class Proxy;
-
-public:
-    std::mutex* const m_updateMtx;
-    Proxy* const m_upperProxy;
-
-    RStatus(stateT state, Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : m_upperProxy(upperProxy)
-        , m_updateMtx(updateMtx)
-        , Status(state, statusMtx)
-    {
-    }
-};
-
-class MissingDependencies : public RStatus {
-public:
-    explicit MissingDependencies(Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : RStatus(MISSING_DEPENDENCIES, upperProxy, statusMtx, updateMtx)
-    {
-    }
-
-    void allFine() override;
-};
-
-class StandBy : public RStatus {
-public:
-    explicit StandBy(Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : RStatus(STAND_BY, upperProxy, statusMtx, updateMtx)
-    {
-    }
-};
-
-class Running : public RStatus {
-public:
-    explicit Running(Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : RStatus(RUNNING, upperProxy, statusMtx, updateMtx)
-    {
-    }
-
-    void somethingIsMissing() override;
-};
-
-class Stopped : public RStatus {
-public:
-    explicit Stopped(Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : RStatus(STOPPED, upperProxy, statusMtx, updateMtx)
-    {
-    }
-};
-
-class Finished : public RStatus {
-public:
-    explicit Finished(Proxy* const upperProxy, std::mutex* const statusMtx, std::mutex* const updateMtx)
-        : RStatus(FINISHED, upperProxy, statusMtx, updateMtx)
-    {
-    }
-};
-
-class Proxy : public ServiceProxy {
-    friend class MissingDependencies;
-    friend class StandBy;
-    friend class Running;
-    friend class Stopped;
-    friend class Finished;
-
-protected:
-    std::thread m_thread;
-    RStatus* m_status;
-    std::mutex m_updateMtx;
-
-    void serviceCycle() override;
-    void autoUpdate() override;
-    void changeState(Status::stateT newState) override;
-    auto checkState() -> Status::stateT;
-    void weave();
-    void cut();
-
-public:
-    Proxy(IService& realService, std::map<std::string, Status::stateT> depsMap);
-    ~Proxy() override;
-
-    auto reportStatus() -> nlohmann::json override;
-};
-} // namespace routineService
 
