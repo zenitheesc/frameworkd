f4619c22c87e047e9a3c739b7d2132708353bea8
Carlos Veras <carlos.craveiro@usp.br>
2021-11-08 03:02:00 -0300

    StaticService & RoutineService: creates both
    Creates the classes StaticService and RoutineService in file
    iservice.hpp, both were inherited from iservice, located in
    the same file.
    
    The idea is to allow function overloading on the method
    "deploy" at the daemon class, when the user decides to choose between
    the two options.

--- src/classes/iservice/iservice.hpp
+++ src/classes/iservice/iservice.hpp
@@ -1,21 +1,20 @@
 
 #pragma once
-#include <mutex>
-#include <string>
 #include <iostream>
+#include <string>
 class IService {
 public:
     /* Id of the service */
     std::string m_serviceId;
 
 protected:
-//	DBusHandler::DBusHandler m_dbus;
+    //	DBusHandler::DBusHandler m_dbus;
 
 public:
   /**
    * @brief	An interface function to be implemented by
-   * 			the user to make the "setup"/configuration
-   * 			of its class' parameter-members.
+   * 		the user to make the "setup"/configuration
+   * 		of its class' parameter-members.
    */
     virtual void setup() = 0;
 
@@ -26,21 +25,21 @@ public:
    *        this function is going to run in a loop, so its 
    *        instructions are going to be executed "routinely".
    */
-    virtual void routine() {}
+    virtual void routine() = 0;
 
   /**
-   * @brief	An interface function to be implemented by
-   * 			the user to execute the needed instructions
-   * 			to safely "destroy" the resources used by the
-   * 			class' previous functions.
+   * @brief An interface function to be implemented by
+   * 		the user to execute the needed instructions
+   * 		to safely "destroy" the resources used by the
+   * 		class' previous functions.
    */
     virtual void destroy() = 0;
 
   /**
    * @brief	The constructor of this interface.
    *
-   * @param	serviceId std::string that is the id related
-   * 			to the instantiated class.
+   * @param serviceId std::string that is the id related
+   * 		to the instantiated class.
    */
 
     explicit IService(std::string serviceId)
@@ -50,8 +49,30 @@ public:
 
   /**
    * @brief	A virtual destructor to ensure
-   * 			inheriance compatibility.
+   * 		inheritance compatibility.
    */
     virtual ~IService() = default;
+};
+
+class StaticService : public IService {
+public:
+    explicit StaticService(std::string serviceId)
+        : IService(serviceId)
+    {
+    }
+
+    void routine() final { }
+
+    ~StaticService() override = default;
+};
+
+class RoutineService : public IService {
+public:
+    explicit RoutineService(std::string serviceId)
+        : IService(serviceId)
+    {
+    }
+
+    ~RoutineService() override = default;
 };
 
