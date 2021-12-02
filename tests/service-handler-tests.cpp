3f48f3d86f4d1398e4259f7577726f919cbec669
Carlos Veras <carlos.craveiro@usp.br>
2021-12-01 21:03:03 -0300

    handler-tests: Implements basic unit tests

--- tests/service-handler-tests.cpp
+++ tests/service-handler-tests.cpp
@@ -1,90 +1,253 @@
 #include "../src/classes/iservice/iservice.hpp"
-#include "../src/classes/routine-service-proxy/routine-service-proxy.hpp"
 #include "../src/classes/service-handler/service-handler.hpp"
-#include "../src/classes/service-proxy/service-proxy.hpp"
-#include "../src/classes/static-service-proxy/static-service-proxy.hpp"
 
+#include <chrono>
 #include <gtest/gtest.h>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <nlohmann/json.hpp>
+#include <stdexcept>
 #include <string>
 #include <thread>
 
-std::mutex sharedDataMtx;
+struct SharedData {
+    bool setupFlag { false };
+    bool destroyFlag { false };
+    int fooCounter { 0 };
+    std::mutex m_mtx;
+};
 
-class ServiceTest : public ::testing::Test {
+void reset(SharedData& data)
+{
+    data.destroyFlag = false;
+    data.setupFlag = false;
+    data.fooCounter = 0;
+}
+
+SharedData staticShared;
+SharedData routineShared;
+
+class ServiceHandlerTests : public ::testing::Test {
+public:
 protected:
-    struct SharedData {
-        bool staticServiceFlag { false };
-        int routineServiceCounter { 0 };
-    };
-
-    class MyService2 : public StaticService {
+    class Service1 : public StaticService {
     public:
-        explicit MyService2(std::string id)
-            : StaticService(id) {};
+        explicit Service1(std::string id)
+            : StaticService { id }
+            , m_attribute1 { 0 }
+            , m_attribute2 { "DEFAULT" } {};
 
-        int coisa1;
-        int coisa2;
+        int m_attribute1;
+        std::string m_attribute2;
 
         void setup() override
         {
-            coisa1 = 1;
-            coisa2 = 4;
+            std::lock_guard<std::mutex> lock { staticShared.m_mtx };
+            staticShared.setupFlag = true;
+
+            m_attribute1++;
+            if (m_attribute2 == "DEFAULT") {
+                m_attribute1++;
+            }
+
+            staticShared.fooCounter++;
         }
 
         void destroy() override
         {
-            std::lock_guard<std::mutex> lock(sharedDataMtx);
-            std::cout << "ENDPOINT HERE" << std::endl;
+            std::lock_guard<std::mutex> lock { staticShared.m_mtx };
+            staticShared.destroyFlag = true;
+            staticShared.fooCounter++;
         }
     };
 
-    class MyService : public RoutineService {
+    class Service2 : public RoutineService {
     public:
-        explicit MyService(std::string id)
-            : RoutineService(id) {};
+        explicit Service2(std::string id)
+            : RoutineService { id }
+            , m_attribute1 { 0 }
+            , m_attribute2 { "DEFAULT" } {};
 
-        int coisa1;
-        int coisa2;
+        int m_attribute1;
+        std::string m_attribute2;
 
         void setup() override
         {
-            coisa1 = 1;
-            coisa2 = 4;
+            std::lock_guard<std::mutex> lock { routineShared.m_mtx };
+            routineShared.setupFlag = true;
+
+            m_attribute1++;
+            if (m_attribute2 == "DEFAULT") {
+                m_attribute1++;
+            }
         }
 
         void routine() override
         {
-            std::lock_guard<std::mutex> lock(sharedDataMtx);
-            std::cout << "Wooow!! Looop!!" << std::endl;
-            std::cout << "coisa1: " << coisa1 << std::endl;
-            std::cout << "coisa2: " << coisa2 << std::endl;
+            std::lock_guard<std::mutex> lock { routineShared.m_mtx };
+            if (routineShared.fooCounter <= 2) {
+                routineShared.fooCounter++;
+            }
         }
 
         void destroy() override
         {
-            std::lock_guard<std::mutex> lock(sharedDataMtx);
+            std::lock_guard<std::mutex> lock { routineShared.m_mtx };
+            routineShared.destroyFlag = true;
         }
     };
-    nlohmann::json m_handler1_config;
-    ServiceTest()
-        : m_handler1_config { { "proxys", { { "NotMyService", { { "Ghost", ServiceProxy::ServiceState::RUNNING }, { "Batata", ServiceProxy::ServiceState::MISSING_DEPENDENCIES } } }, { "MyService", {} } } } }
-        , m_testHandler { m_handler1_config } {};
-
-    ServiceHandler m_testHandler;
 };
 
-TEST_F(ServiceTest, RequestOneProxyState)
+TEST_F(ServiceHandlerTests, BasicRequestProxyState)
 {
-    MyService2 m_service2("MyService");
-    m_testHandler.buildServiceProxy(m_service2);
-    nlohmann::json expectedOutput = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "MyService" } };
-    EXPECT_EQ(m_testHandler.getProxyState("MyService"), expectedOutput);
-    //testHandler.run();
-    //EXPECT_EQ(testHandler.getProxyState("MyService"), ServiceProxy::ServiceState::UNINITIALIZED);
+    nlohmann::json configJSON = { { "proxys", { { "Camera", { { "Energy", ServiceProxy::ServiceState::RUNNING }, { "Emergency", ServiceProxy::ServiceState::MISSING_DEPENDENCIES } } }, { "Energy", {} } } } };
+
+    ServiceHandler handler(configJSON);
+
+    Service1 camera("Camera");
+
+    handler.buildServiceProxy(camera);
+
+    nlohmann::json expectedState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Camera" } };
+
+    auto result = handler.getAllProxyState();
+    EXPECT_EQ(result["Camera"], expectedState);
+
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expectedState);
 }
+
+TEST_F(ServiceHandlerTests, Run)
+{
+    nlohmann::json configJSON = { { "proxys", { { "Camera", { { "Energy", ServiceProxy::ServiceState::RUNNING }, { "Emergency", ServiceProxy::ServiceState::MISSING_DEPENDENCIES } } }, { "Energy", {} } } } };
+
+    ServiceHandler handler(configJSON);
+
+    Service1 energy("Energy");
+
+    handler.buildServiceProxy(energy);
+
+    nlohmann::json expectedState = { { "State", ServiceProxy::ServiceState::UNINITIALIZED }, { "serviceId", "Energy" } };
+
+    handler.run();
+
+    auto result = handler.getAllProxyState();
+
+    EXPECT_EQ(result["Energy"], expectedState);
+}
+
+TEST_F(ServiceHandlerTests, FullRequestProxyState)
+{
+    nlohmann::json configJSON = { { "proxys", { { "Camera", { { "Radio", ServiceProxy::ServiceState::RUNNING }, { "TermicShield", ServiceProxy::ServiceState::MISSING_DEPENDENCIES } } }, { "Energy", {} } } } };
+
+    ServiceHandler handler(configJSON);
+
+    Service1 camera("Camera");
+
+    Service1 energy("Energy");
+
+    handler.buildServiceProxy(camera);
+    handler.buildServiceProxy(energy);
+
+    nlohmann::json expCameraState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Camera" } };
+    nlohmann::json expEnergyState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Energy" } };
+
+    auto result = handler.getAllProxyState();
+    EXPECT_EQ(result["Camera"], expCameraState);
+    EXPECT_EQ(result["Energy"], expEnergyState);
+
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+
+    result = handler.getProxyState("Energy");
+    EXPECT_EQ(result, expEnergyState);
+
+    handler.run();
+
+    expEnergyState = { { "State", ServiceProxy::ServiceState::UNINITIALIZED }, { "serviceId", "Energy" } };
+
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+    EXPECT_EQ(routineShared.setupFlag, false);
+
+    result = handler.getProxyState("Energy");
+    EXPECT_EQ(result, expEnergyState);
+
+    reset(routineShared);
+}
+
+TEST_F(ServiceHandlerTests, ChangeProxyDependency)
+{
+    nlohmann::json configJSON = { { "proxys", { { "Camera", { { "Radio", ServiceProxy::ServiceState::RUNNING }, { "TermicShield", ServiceProxy::ServiceState::MISSING_DEPENDENCIES } } }, { "Energy", {} } } } };
+
+    ServiceHandler handler(configJSON);
+
+    Service2 camera("Camera");
+
+    Service1 energy("Energy");
+
+    handler.buildServiceProxy(camera);
+    handler.buildServiceProxy(energy);
+    
+    ASSERT_THROW(handler.changeDependencyState("Energy", "THIS", ServiceProxy::ServiceState::RUNNING), std::invalid_argument);
+
+    handler.run();
+    
+    nlohmann::json expCameraState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Camera" } };
+    handler.changeDependencyState("Camera", "TermicShield", ServiceProxy::ServiceState::MISSING_DEPENDENCIES);
+    auto result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+    
+    expCameraState = { { "State", ServiceProxy::ServiceState::RUNNING }, { "serviceId", "Camera" } };
+    handler.changeDependencyState("Camera", "Radio", ServiceProxy::ServiceState::RUNNING);
+    
+    handler.updateServiceProxy("Camera");
+ 
+    std::chrono::seconds fewSeconds(3);
+    std::this_thread::sleep_for(fewSeconds); 
+    
+    handler.updateServiceProxy("Camera");
+   
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+
+    
+
+    handler.changeDependencyState("Camera", "Radio", ServiceProxy::ServiceState::MISSING_DEPENDENCIES);
+    handler.updateServiceProxy("Camera");
+    
+    expCameraState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Camera" } };
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+
+    EXPECT_EQ(routineShared.setupFlag, true);
+    EXPECT_EQ(routineShared.fooCounter, 3);
+    EXPECT_EQ(routineShared.setupFlag, true);
+
+    /*nlohmann::json expEnergyState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Energy" } };
+
+
+    expEnergyState = { { "State", ServiceProxy::ServiceState::UNINITIALIZED }, { "serviceId", "Energy" } };
+    expCameraState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Camera" } };
+    expEnergyState = { { "State", ServiceProxy::ServiceState::MISSING_DEPENDENCIES }, { "serviceId", "Energy" } };
+
+
+    result = handler.getProxyState("Camera");
+    EXPECT_EQ(result, expCameraState);
+    EXPECT_EQ(routineShared.setupFlag, false);
+
+    result = handler.getProxyState("Energy");
+    EXPECT_EQ(result, expEnergyState);
+*/
+    reset(routineShared);
+
+}
+
+TEST_F(ServiceHandlerTests, UpdateProxys)
+{
+}
+
 /*
  * public:
     auto getProxyState(std::string serviceId) -> nlohmann::json;
